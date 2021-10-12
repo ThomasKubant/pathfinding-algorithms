1c2096a9a9b1c3f285db25e1f3ce326c710ce9d1
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-10-12 11:18:09 -0700

    initial dijkstra algorithm and visualization finished

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Pathfinder from './Components/Pathfinder/Pathfinder'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathfinder />
+    </div>
+  );
+}
+
+export default App;
